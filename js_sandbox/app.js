f6a0d503a8e025398cbd6af351c72e9fedf9e301
John Mendes <johnhenrymendes@gmail.com>
2019-09-18 21:25:30 -0400

    event bubbling and delegation

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -480,3 +480,41 @@ function runEvent(e) {
   // heading.innerText = e.target.value;
 }
 */
+/********** Event Bubbling and Delegation  **********/
+
+/*
+//BUBBLE
+ document.querySelector('.card-title').addEventListener('click', function(){
+   console.log('Card Title');
+ });
+
+ document.querySelector('.card-content').addEventListener('click', function(){
+  console.log('Card Content');
+});
+
+document.querySelector('.card').addEventListener('click', function(){
+  console.log('Card');
+});
+
+document.querySelector('.col').addEventListener('click', function(){
+  console.log('Col');
+});
+
+
+//DELEGATION
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+  // if(e.target.parentElement.className === 'delete-item secondary-content'){
+  //   console.log('Item deleted');
+  // }
+
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    console.log('Item deleted');
+    e.target.parentElement.parentElement.remove();
+  }
+}
+*/
+
+/********** Local and Session storage  **********/
