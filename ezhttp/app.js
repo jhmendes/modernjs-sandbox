6ca5c1017224edd6adb24e808d95cbdcc3b6b4a4
John Mendes <johnhenrymendes@gmail.com>
2019-10-02 22:12:48 -0400

    http library complete

--- ezhttp/app.js
+++ ezhttp/app.js
@@ -4,7 +4,55 @@ const http = new easyHTTP;
 
 //Get Posts;
 
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, response) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, response) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+//Get a single post
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Create data
+
+const data = {
+  title: 'Custom post',
+  body: 'This is a test post!!!'
+}
+
+
+//Create a post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//         console.log(err)
+//       } else {
+//         console.log(post);
+//       }
+// });
+
+//Update a post 
+
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//   if(err) {
+//         console.log(err)
+//       } else {
+//         console.log(post);
+//       }
+// });
+
+//Delete a post 
+
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
   if(err) {
     console.log(err)
   } else {
--- ezhttp/ezhttp.js
+++ ezhttp/ezhttp.js
@@ -10,10 +10,11 @@ easyHTTP.prototype.get = function(url,callback) {
   this.http.open('GET', url, true);
 
   let self = this;
+  
   // console.log(this);
   this.http.onload = function() {
-    // console.log(typeof this);
-    //this inside this function pertains to the function call - 
+    console.log(this);
+    //this inside this function pertains to the function call - the XML request object doesn't have .http as a method
     if(self.http.status === 200) {
       callback(null, self.http.responseText);
     } else {
@@ -25,5 +26,60 @@ easyHTTP.prototype.get = function(url,callback) {
   this.http.send();
 }
 //Make a HTTP POST request
+
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+
+    this.http.onload = function() {
+      callback(null, self.http.responseText);
+    }
+
+
+
+
+  this.http.send(JSON.stringify(data));
+}
+
 //Make a HTTP PUT request
+
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+
+    this.http.onload = function() {
+      callback(null, self.http.responseText);
+    }
+
+
+
+
+  this.http.send(JSON.stringify(data));
+}
 //Make a HTTP DELETE request
+
+easyHTTP.prototype.delete = function(url,callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+  
+  // console.log(this);
+  this.http.onload = function() {
+    console.log(this);
+    //this inside this function pertains to the function call - the XML request object doesn't have .http as a method
+    if(self.http.status === 200) {
+      callback(null, 'Post deleted.');
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  }
+
+
+  this.http.send();
+}
